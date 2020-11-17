df7373579a7a9ef8a1175cafa307b537e3e0a51b
Phil Mossman <mossmanpik@iname.com>
2020-11-17 20:46:09 +0000

    First attempt at record_preferences() function
    Set function to loop through rank pairs and update preferences array
    accordingly.

--- tideman.c
+++ tideman.c
@@ -106,7 +106,10 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    // TODO
+    for (int i = 0; i < candidate_count - 1; i++)
+    {
+        preferences[ranks[i]][ranks[i + 1]]++;
+    }
     return;
 }
 
